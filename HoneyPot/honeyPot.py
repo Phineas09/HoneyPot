87cfe5e74a8473665f1cb877745c30e0eb128c7c
Phineas09 <claudiu.ghenea15@yahoo.ro>
2020-11-17 10:42:30 +0200

    First commit after moving to another server
    Signed-off-by: Phineas09 <claudiu.ghenea15@yahoo.ro>

---
+++ HoneyPot/honeyPot.py
@@ -0,0 +1,64 @@
+import pyshark
+import mysql.connector
+import mysqlx
+from simpleMySql import SimpleMysql
+
+
+
+class CustomPacket:
+
+    packetStack = None
+    packetCount = 0
+
+
+    def __init__(self):
+        self.packetCount = 0
+        self.packetStack = []
+
+
+    def processPackets(self, packet):
+        self.packetCount = self.packetCount + 1
+        if(packet.layers[-1].layer_name == "http"):
+            #print(packet.pretty_print())
+            self.packetStack.append(packet)
+            #
+            #print(packet.layers[1].field_names)
+            print(f"Http packet from {packet.layers[1].get_field('host')}\t to  {packet.layers[1].get_field('dst')}")
+            #print(packet.layers[-1].field_names)
+            #I want only the attacker packets
+            if(packet.layers[1].get_field('dst') == "192.168.1.97"):
+                print(packet.layers[-1].get_field('request_uri'))
+                print(packet.layers[-1].get_field('request_full_uri'))
+                print(packet.layers[-1].get_field('request_method'))
+                print("Delim---------------------------------------")
+
+
+
+capture = pyshark.LiveCapture(interface='ens33', bpf_filter='tcp')
+customPacket = CustomPacket()
+
+capture.apply_on_packets(customPacket.processPackets)
+
+'''
+db = SimpleMysql(
+    host="192.168.1.235",
+    port=3306,
+    user="honeyMonitor",
+    passwd="Claudiu147!$&",
+    db="honeyData",
+	keep_alive=True # try and reconnect timedout mysql connections?
+)
+
+book = db.getAll("test", ["name"])
+
+db.update("test",
+	{"a": 250},
+	("id=%s AND b=%s", (12, 4))
+)
+
+db.commit()
+print(book)
+book = db.getAll("test", ["name"])
+print(book)
+
+'''
